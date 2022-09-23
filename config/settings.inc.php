737f075eb81d6d7177c84101166bc095713e867b
nhust <worfut@hotmail.fr>
2022-09-23 15:19:39 +0200

    Config

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'test');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', 'root');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'PNPZyw8chaZd5fRphmn8gSp957xvnn5GAxDQwz4JfBHmkP6UHJPeMHH0');
+define('_COOKIE_IV_', 'ob6mpdJV');
+define('_PS_CREATION_DATE_', '2022-09-23');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.24');
+define('_RIJNDAEL_KEY_', '6EDdTkP3b0TOKbUiDCNW9JiBOpFUHsgw');
+define('_RIJNDAEL_IV_', 'x6NhzARifJmod+qORV9HtA==');
